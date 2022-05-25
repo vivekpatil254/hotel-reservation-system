0487ff3241a4f046b21880fe7714472207dd6f49
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 <Vivekpatil254@gmail.com>
2022-05-25 09:16:25 +0530

    UC2 first commit

--- src/com/bridgelabz/Hotel.java
+++ src/com/bridgelabz/Hotel.java
@@ -1,18 +1,62 @@
 package com.bridgelabz;
 
+
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.ChronoUnit;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.util.concurrent.TimeUnit.DAYS;
+
 public class Hotel {
-    String hotelName;
-    int regulerCustomerRate;
-         public Hotel(String name, int rate)
-            {
-                hotelName=name;
-                regulerCustomerRate=rate;
-            }
-   public void display()
-   {
-       System.out.println("\nHotel name            : "+hotelName);
-       System.out.println("Reguler Customer rate : "+regulerCustomerRate);
-   }
+    Scanner scanner = new Scanner(System.in);
+    private String hotelName, fromDate, toDate;
+    private int regulerCustomerRate;
+    private LocalDate fromDateObject;
+    private LocalDate toDateObject;
+
+    public Hotel(String name, int rate) {
+        hotelName = name;
+        regulerCustomerRate = rate;
+    }
+
+    public void display() {
+        System.out.println("\nHotel name            : " + hotelName);
+        System.out.println("Reguler Customer rate : " + regulerCustomerRate);
+    }
 
 
+    public void input() {
+        System.out.println("Enter the start date to book : \n NOTE - The date should be in given formatte 'YYYY-MM-DD'");
+        fromDate = scanner.next();
+        System.out.println("Enter the end date to book : \n NOTE - The date should be in given formatte 'YYYY-MM-DD'");
+        toDate = scanner.next();
+        try {
+            this.fromDateObject = LocalDate.parse(fromDate);
+            this.toDateObject = LocalDate.parse(toDate);
+            System.out.println("object  'date' : " + fromDateObject);
+            System.out.println("object  'date' : " + toDateObject);
+        } catch (Exception e) {
+            System.out.println("exception genrated " + e);
+        }
+    }
+
+    public long findAmmount() {
+        long numberOfDays = 0, totalAmmount;
+        try {
+
+            numberOfDays = ChronoUnit.DAYS.between(fromDateObject,toDateObject);
+            System.out.println(numberOfDays);
+
+        } catch (Exception e) {
+            System.out.println(" exeception genrated in find ammount" + e);
+        }
+        totalAmmount = numberOfDays * regulerCustomerRate;
+        return totalAmmount;
+
+    }
 }
--- src/com/bridgelabz/HotelReservationMain.java
+++ src/com/bridgelabz/HotelReservationMain.java
@@ -5,12 +5,23 @@ import javax.print.attribute.HashPrintJobAttributeSet;
 public class HotelReservationMain {
 
     public static void main(String[] args) {
+        long ammountLakwood, ammountBridgewood, ammountRidgewood;
         System.out.println("Welcome to Hotel reservation System...");
-        Hotel hotel1 = new Hotel("Lakewood",110);
-        Hotel hotel2 = new Hotel("Bridgewood",160);
-        Hotel hotel3 = new Hotel("Ridgewood",220);
+        Hotel hotel1 = new Hotel("Lakewood", 110);
+        Hotel hotel2 = new Hotel("Bridgewood", 160);
+        Hotel hotel3 = new Hotel("Ridgewood", 220);
         hotel1.display();
         hotel2.display();
         hotel3.display();
+        hotel1.input();
+        ammountLakwood = hotel1.findAmmount();
+        ammountBridgewood = hotel2.findAmmount();
+        ammountRidgewood = hotel3.findAmmount();
+        if (ammountLakwood > ammountBridgewood && ammountRidgewood > ammountBridgewood)
+            System.out.println("The Hotel Bridgewood is the cheapest hotel. Requires ammount of "+ammountBridgewood+"$");
+        else if(ammountLakwood<ammountRidgewood)
+            System.out.println("The Hotel Lakewood is the cheapest hotel. Requires ammount of "+ammountLakwood+"$");
+        else
+            System.out.println("The Hotel Ridgewood is the cheapest hotel. Requires ammount of "+ammountRidgewood+"$");
     }
 }
