bac379384eba0e996cd7a97b449935fa1c3f1527
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 <Vivekpatil254@gmail.com>
2022-05-26 10:20:18 +0530

    UC2 after code reduce commit

--- src/com/bridgelabz/Hotel.java
+++ src/com/bridgelabz/Hotel.java
@@ -1,29 +1,27 @@
 package com.bridgelabz;
-
-
-import java.time.chrono.ChronoLocalDate;
 import java.time.temporal.ChronoUnit;
-import java.text.DateFormat;
 import java.time.LocalDate;
-import java.time.Period;
-import java.time.temporal.Temporal;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Scanner;
 
-import static java.util.concurrent.TimeUnit.DAYS;
-
 public class Hotel {
-    private long totalAmmount;
     Scanner scanner = new Scanner(System.in);
+    private long totalAmmount;
     private String hotelName;
-    private String fromDate;
-    private String toDate;
     private int regulerCustomerRate;
 
     public Hotel(String name, int rate) {
         hotelName = name;
         regulerCustomerRate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "totalAmmount=" + totalAmmount +
+                ", scanner=" + scanner +
+                ", hotelName='" + hotelName + '\'' +
+                ", regulerCustomerRate=" + regulerCustomerRate +
+                '}';
     }
 
     public long getTotalAmmount() {
@@ -50,22 +48,6 @@ public class Hotel {
         this.hotelName = hotelName;
     }
 
-    public String getFromDate() {
-        return fromDate;
-    }
-
-    public void setFromDate(String fromDate) {
-        this.fromDate = fromDate;
-    }
-
-    public String getToDate() {
-        return toDate;
-    }
-
-    public void setToDate(String toDate) {
-        this.toDate = toDate;
-    }
-
     public int getRegulerCustomerRate() {
         return regulerCustomerRate;
     }
@@ -79,18 +61,10 @@ public class Hotel {
         System.out.println("Reguler Customer rate : " + regulerCustomerRate);
     }
 
+    public long findAmmount(long numberOfDays) {
 
-
-
-    public long findAmmount(LocalDate fromDateObject, LocalDate toDateObject) {
-        long numberOfDays = 0;
         try {
-
-            numberOfDays = ChronoUnit.DAYS.between(fromDateObject, toDateObject);
-            System.out.println(numberOfDays);
             totalAmmount = numberOfDays * regulerCustomerRate;
-            System.out.println(totalAmmount);
-
         } catch (Exception e) {
             System.out.println(" exeception genrated in find ammount" + e);
         }
--- src/com/bridgelabz/HotelReservationMain.java
+++ src/com/bridgelabz/HotelReservationMain.java
@@ -1,53 +1,47 @@
 package com.bridgelabz;
 
-import javax.print.attribute.HashPrintJobAttributeSet;
 import java.time.LocalDate;
-import java.util.Scanner;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class HotelReservationMain {
 
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //long ammountLakwood, ammountBridgewood, ammountRidgewood;
         System.out.println("Welcome to Hotel reservation System...");
-        Hotel hotel1 = new Hotel("Lakewood", 110);
-        Hotel hotel2 = new Hotel("Bridgewood", 160);
-        Hotel hotel3 = new Hotel("Ridgewood", 220);
-        hotel1.display();
-        hotel2.display();
-        hotel3.display();
+        List<Hotel> hotelList = new ArrayList<Hotel>();
+        hotelList.add(new Hotel("Lakewood", 180));
+        hotelList.add(new Hotel("Bridgewood", 160));
+        hotelList.add(new Hotel("Ridgewood", 220));
+        hotelList.forEach(hotel -> {
+            hotel.display();
+        });
 
 
         LocalDate fromDateObject;
         LocalDate toDateObject;
         try {
-        System.out.println("Enter the start date to book : \n NOTE - The date should be in given formatte 'YYYY-MM-DD'");
-        fromDateObject = LocalDate.parse(scanner.next());
-        System.out.println("Enter the end date to book : \n NOTE - The date should be in given formatte 'YYYY-MM-DD'");
-        toDateObject = LocalDate.parse(scanner.next());
-
-
-
-            System.out.println("object  'date' : " + fromDateObject);
-            System.out.println("object  'date' : " + toDateObject);
-            hotel1.findAmmount(fromDateObject,toDateObject);
-            hotel2.findAmmount(fromDateObject,toDateObject);
-            hotel3.findAmmount(fromDateObject,toDateObject);
+            System.out.println("Enter the start date to book : \n NOTE - The date should be in given formatte 'YYYY-MM-DD'");
+            fromDateObject = LocalDate.parse(scanner.next());
+            System.out.println("Enter the end date to book : \n NOTE - The date should be in given formatte 'YYYY-MM-DD'");
+            toDateObject = LocalDate.parse(scanner.next());
+            long dayCount = countDays(fromDateObject, toDateObject);
+            hotelList.forEach(hotel -> {
+                hotel.findAmmount(dayCount);
+            });
         } catch (Exception e) {
             System.out.println("exception genrated " + e);
         }
-
-
-
-
-        if (hotel1.getTotalAmmount() > hotel2.getTotalAmmount() && hotel3.getTotalAmmount() > hotel2.getTotalAmmount())
-            System.out.println("The Hotel Bridgewood is the cheapest hotel. Requires ammount of " + hotel2.getTotalAmmount() + "$");
-        else if (hotel1.getTotalAmmount() < hotel3.getTotalAmmount())
-            System.out.println("The Hotel Lakewood is the cheapest hotel. Requires ammount of " + hotel1.getTotalAmmount() + "$");
-        else
-            System.out.println("The Hotel Ridgewood is the cheapest hotel. Requires ammount of " + hotel3.getTotalAmmount() + "$");
+        Optional<Hotel> cheapHotel = hotelList.stream().collect(Collectors.minBy(Comparator.comparing(Hotel::getTotalAmmount)));
+        System.out.println("the chepest hotel is '" + cheapHotel.get().getHotelName() + "' and it costs '" + cheapHotel.get().getTotalAmmount() + "'");
     }
 
-
+    public static long countDays(LocalDate fromDateObject, LocalDate toDateObject) {
+        long numberOfDays = 0;
+        numberOfDays = ChronoUnit.DAYS.between(fromDateObject, toDateObject);
+        System.out.println(numberOfDays);
+        return numberOfDays;
+    }
 }
